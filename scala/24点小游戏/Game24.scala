b52eb27f3430a5edb7d6ed46b5c00744b9244bf2
Ericjeff <w3045872817@163.com>
2017-12-19 13:49:01 +0800

    实现加减乘除

--- scala/24点小游戏/Game24.scala
+++ scala/24点小游戏/Game24.scala
@@ -3,24 +3,33 @@ package com.game24point.rg
 object Game24 {
   def main(args: Array[String]): Unit = {
 
-    println(eval("23+234+234"))
+    println(eval("23+234+234-234/234+234*234"))
   }
 
+  //模式匹配提取
   def eval(str:String):Int = str match{
-
     case Add(expr1,expr2) => eval(expr1)+eval(expr2)
+    case Subtract(expr1,expr2) => eval(expr1)-eval(expr2)
+    case Multiply(expr1,expr2) => eval(expr1)*eval(expr2)
+    case Divide(expr1,expr2) => eval(expr1)/eval(expr2)
     case _ => str toInt
   }
 }
 
-//先写一个加
-object Add{
+//加减乘除的主体
+trait BinaryOp{
+  val op:String
+  def unapply(str:String):Option[(String,String)]={
 
-  def unapply(str: String): Option[(String,String)] ={
-
-    val index = str indexOf("+")
+    val index = str indexOf(op)
     if(index == -1) None
-    else
-      Some(str.substring(0,index),str.substring(index+1))
+    else Some(str.substring(0,index),str.substring(index+1))
   }
 }
+
+//加减乘除实现
+object Add extends {val op="+"} with BinaryOp
+object Subtract extends {val op="-"} with BinaryOp
+object Multiply extends {val op="*"} with BinaryOp
+object Divide extends {val op="/"} with BinaryOp
+
